57c2f6d5b99949e69b0d150a0a37e5159db3282e
Shakthi <shaqfreak1634@gmail.com>
2026-07-19 04:38:09 +0530

    next.config.ts changes

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -3,11 +3,15 @@ import type { NextConfig } from "next";
 const BACKEND_URL = process.env.BACKEND_API_URL || "http://127.0.0.1:8003";
 
 const nextConfig: NextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+
   async rewrites() {
     return [
       {
         source: "/api/:path*",
-        destination: `${BACKEND_URL}/api/:path*`, // Proxy to backend
+        destination: `${BACKEND_URL}/api/:path*`,
       },
     ];
   },
